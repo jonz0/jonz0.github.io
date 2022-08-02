77c66ca08633e15e37d21b0692313132b56f6ee1
jonz0 <ijonluu@gmail.com>
2022-08-02 01:10:18 -0700

    feat: js and jquery imports, gitlet content finished

---
+++ js/script-main.js
@@ -0,0 +1,8 @@
+function showGitletCommands() {
+    var id = document.getElementById("show-hide");
+    if (id.style.display === "none") {
+        id.style.display = "block";
+    } else {
+        id.style.display = "none";
+    }
+}
