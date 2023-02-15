a4aec233dfcb9a0e0af59fa4d1417aee5c313249
jonz0 <ijonluu@gmail.com>
2023-02-14 16:02:34 -0800

    feat: add carousel prototype

--- carousel-script.js
+++ carousel-script.js
@@ -1,7 +1,23 @@
 const carousel = document.querySelector(".carousel");
 
-const deragging = (e) => {
+const isDragStart = false;
+
+const dragStop = () => {
+  isDragStart = false;
+};
+
+const dragStart = () => {
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
   carousel.scrollLeft = e.pageX;
 };
 
+carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
